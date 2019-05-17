7c92b6304658bcaa98ebcc5938cc6460b3bab764
sho <sskmy1024.y@gmail.com>
2019-05-17 12:06:45 +0900

    complete users model

--- src/app.py
+++ src/app.py
@@ -2,27 +2,69 @@
 
 from flask import Flask, request, make_response, redirect, abort
 from users import Users
+import json
 
 Users.initDatabase()
 
-Users.register("hogehoge", "fugafuga", "/private,/hogehoge")
+Users.register('admin', 'root', '/')
 
 app = Flask(__name__)
 
 @app.route('/auth/users', methods=['GET', 'POST'])
-def register():    
-    if request.method == 'POST':
-        # ユーザ情報を追加
-        result = Users.register(request.form['name'], request.form['password'], request.form['pathlist'])
-        if result:
-            # response = make_response(redirect('/'))
-            # return response
-            return 'success!', 201
-        else:
-            return 'Conflict', 409
-        
+def register():
+    if is_auth()[1] == 200:
+        if request.method == 'POST':
+            # ユーザ情報を追加
+            params = request.get_json()
+            result = Users.register(params['name'], params['password'], params['pathlist'])
+            if result:
+                return json.dumps(result.toJSON()), 201
+            else:
+                return json.dumps('409 Conflict'), 409
+            
+        elif request.method == 'GET':
+            return Users.getUserLists(), 200
     else:
-        return '', 200
+        return json.dumps('403 Forbidden'), 403
+
+
+@app.route('/auth/users/<int:usersid>', methods=['GET', 'PUT', 'DELETE'])
+def show_users(usersid):
+    if is_auth()[1] == 200:
+        if request.method == 'GET':
+            user = Users.findById(usersid)
+            if user:
+                return json.dumps(user.toJSON())
+            else:
+                return json.dumps('User is not found'), 404
+
+        elif request.method == 'PUT':     
+            user = Users.findById(usersid)
+            if user:
+                params = request.get_json()
+                user.setName(params['name']) if "name" in params else None
+                user.setPassword(params['password']) if "password" in params else None
+                user.setPathList(params['pathlist']) if "pathlist" in params else None
+
+                if user.update():
+                    return '', 204
+                else:
+                    return json.dumps('409 Conflict'), 409
+            else:
+                return json.dumps('User is not found'), 404
+            
+        elif request.method == 'DELETE':
+            user = Users.findById(usersid)
+            if user.delete():
+                return '', 204
+            else:
+                return json.dumps('409 Conflict'), 409
+
+                
+    else:
+        return json.dumps('403 Forbidden'), 403
+    
+
 
 @app.route('/auth/is_auth')
 def is_auth():
--- src/users.py
+++ src/users.py
@@ -1,12 +1,14 @@
 import hashlib
 import re
 import sqlite3
+import json
 
 class Users():
 
   dbpath = "users.db"
   
-  def __init__(self, name, password, pathlist, ishashed=False):
+  def __init__(self, userid, name, password, pathlist, ishashed=False):
+    self.id = userid
     self.name = name
     self.pass_hash = password if ishashed else hashlib.sha512(password.encode('utf-8')).hexdigest()
     self.pathlist = re.split(',\r*\n*', pathlist)
@@ -14,12 +16,52 @@ class Users():
   def auth(self, password):
     return self.pass_hash == hashlib.sha512(password.encode('utf-8')).hexdigest()
 
-  def getUsername(self):
+  def setName(self, name):
+    self.name = name
+
+  def getName(self):
     return self.name
+
+  def setPassword(self, password, ishashed=False):
+    self.pass_hash = password if ishashed else hashlib.sha512(password.encode('utf-8')).hexdigest()
+
+  def setPathList(self, pathlist):
+    self.pathlist = re.split(',\r*\n*', pathlist)
 
   def getPathList(self):
     return self.pathlist
-    
+
+  def toJSON(self):
+    return {'id': self.id, 'name': self.name, 'pathlist': self.pathlist}
+
+  def update(self):
+    # DB connection
+    connection = sqlite3.connect(self.dbpath)
+    cursor = connection.cursor()
+
+    try:
+      cursor.execute("UPDATE users SET name=?, password=?, pathlist=? WHERE id=?", (self.name, self.pass_hash, ','.join(self.pathlist), self.id))
+    except sqlite3.Error as e:
+      print('sqlite3.Error occurred:', e.args[0])
+      return False
+
+    connection.commit()
+    connection.close()
+    return self
+
+  def delete(self):
+    connection = sqlite3.connect(self.dbpath)
+    cursor = connection.cursor()
+    try:
+      cursor.execute("DELETE FROM users WHERE id=?", (self.id,))
+    except sqlite3.Error as e:
+      print('sqlite3.Error occurred:', e.args[0])
+      return False
+
+    connection.commit()
+    connection.close()
+    return True
+
   @classmethod
   def initDatabase(cls, path="users.db"):
     cls.dbpath = path
@@ -37,25 +79,29 @@ class Users():
 
   @classmethod
   def register(cls, name, password, pathlist):
-
     if cls.find(name):
       return False
     else:
       pass_hash = hashlib.sha512(password.encode('utf-8')).hexdigest()
-    
+      data = False
       # DB connection
       connection = sqlite3.connect(cls.dbpath)
+      connection.row_factory = sqlite3.Row
       cursor = connection.cursor()
       try:
         cursor.execute("INSERT INTO users(name, password, pathlist) VALUES (:name, :password, :pathlist)", {'name': name, 'password': pass_hash, 'pathlist': pathlist})
-        u = cursor.fetchone()    
+        cursor.execute("SELECT * FROM users WHERE name=?", (name,))
+        u = cursor.fetchone()
+        if u:
+          data = cls(u['id'], u['name'], u['password'], u['pathlist'], True)
+  
       except sqlite3.Error as e:
         print('sqlite3.Error occurred:', e.args[0])
         return False
 
       connection.commit()
       connection.close()
-      return True
+      return data
 
   @classmethod
   def find(cls, name):
@@ -69,7 +115,26 @@ class Users():
       cursor.execute("SELECT * FROM users WHERE name=?", (name,))
       u = cursor.fetchone()
       if u:
-        data = cls(u['name'], u['password'], u['pathlist'], True)
+        data = cls(u['id'], u['name'], u['password'], u['pathlist'], True)
+    except sqlite3.Error as e:
+      print('sqlite3.Error occurred:', e.args[0])
+
+    connection.close()
+    return data
+  
+  @classmethod
+  def findById(cls, userid):
+    data = False
+    # DB connection
+    connection = sqlite3.connect(cls.dbpath)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    try:
+      cursor.execute("SELECT * FROM users WHERE id=?", (userid,))
+      u = cursor.fetchone()
+      if u:
+        data = cls(u['id'], u['name'], u['password'], u['pathlist'], True)
     except sqlite3.Error as e:
       print('sqlite3.Error occurred:', e.args[0])
 
@@ -85,13 +150,13 @@ class Users():
     cursor = connection.cursor()
 
     try:
-      cursor.execute("SELECT * FROM users WHERE name=?", (name,))
+      cursor.execute("SELECT * FROM users")
       us = cursor.fetchall()
       for u in us:
-        data.append(cls(u['name'], u['password'], u['pathlist'], True))
-      
+        data.append(cls(u['id'], u['name'], u['password'], u['pathlist'], True).toJSON())
+
     except sqlite3.Error as e:
       print('sqlite3.Error occurred:', e.args[0])
 
     connection.close()
-    return data
+    return json.dumps(data)
